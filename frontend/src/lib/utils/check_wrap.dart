26c6a81e2e23e722097a60a34f8c8adabd804987
Ilya Kochankov <fakafafakafa@mail.ru>
2021-12-02 19:25:02 +0300

    Fix design (#83)
    * bug: fix design
    
    * fix
    
    * fix linters

--- frontend/src/lib/utils/check_wrap.dart
+++ frontend/src/lib/utils/check_wrap.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 bool checkWrap(BuildContext context) {
-  return !(MediaQuery.of(context).size.width >= 489);
+  Size headerTextSize =
+      _textSize('Tonal assistant', Theme.of(context).textTheme.headline1!);
+  return headerTextSize.width >= MediaQuery.of(context).size.width - 7;
+}
+
+Size _textSize(String text, TextStyle style) {
+  final TextPainter textPainter = TextPainter(
+      text: TextSpan(text: text, style: style),
+      maxLines: 1,
+      textDirection: TextDirection.ltr)
+    ..layout(minWidth: 0, maxWidth: double.infinity);
+  return textPainter.size;
 }
