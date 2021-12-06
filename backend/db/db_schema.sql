96e8744b49ffa766d32e26a0944273d676963e71
ilyakochankov <ilyakochankov@yandex.ru>
2021-12-06 05:31:28 +0300

    add updated diagram

--- backend/db/db_schema.sql
+++ backend/db/db_schema.sql
@@ -2,32 +2,57 @@ CREATE SCHEMA IF NOT EXISTS vendor;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DO $$
-BEGIN
-    CREATE TYPE vendor.product_type AS ENUM ( 'TONAL_CREAM'
-);
-EXCEPTION
-WHEN duplicate_object THEN
-    NULL;
-END $$;
+DO
+$$
+    BEGIN
+        CREATE TYPE vendor.product_type AS ENUM ( 'TONAL_CREAM'
+            );
+    EXCEPTION
+        WHEN duplicate_object THEN
+            NULL;
+    END
+$$;
 
-CREATE TABLE IF NOT EXISTS vendor.vendor (
-        id uuid DEFAULT uuid_generate_v4 () NOT NULL CONSTRAINT vendor_vendor_pkey PRIMARY KEY,
-        name text NOT NULL,
-        url varchar(200)
+create table if not exists vendor.vendor
+(
+    id   uuid default uuid_generate_v4() not null
+        constraint vendor_vendor_pkey
+            primary key,
+    name text                            not null,
+    url  varchar(200)
 );
 
-CREATE TABLE IF NOT EXISTS vendor.vendor_color (
-        id uuid DEFAULT uuid_generate_v4 () NOT NULL CONSTRAINT vendor_color_pk PRIMARY KEY,
-        name text NOT NULL,
-        color varchar(18) NOT NULL
+create table if not exists vendor.vendor_color
+(
+    id        uuid default uuid_generate_v4() not null
+        constraint vendor_color_pk
+            primary key,
+    name      text                            not null,
+    color     varchar(18)                     not null,
+    vendor_id uuid                            not null
+        constraint vendor_color_vendor_id_fk
+            references vendor.vendor
+            on delete cascade
 );
 
-CREATE TABLE IF NOT EXISTS vendor.product (
-        id uuid DEFAULT uuid_generate_v4 () NOT NULL CONSTRAINT vendor_product_pkey PRIMARY KEY,
-        name text NOT NULL,
-        TYPE vendor.product_type NOT NULL,
-        vendor_id uuid NOT NULL CONSTRAINT vendor_product_vendor_id_fk_vendor_vendor_id REFERENCES vendor.vendor ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
-        color_id uuid NOT NULL CONSTRAINT vendor_product_color_id_fk_vendor_vendor_color_id REFERENCES vendor.vendor_color ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
-        url text
+create table if not exists vendor.product
+(
+    id        uuid default uuid_generate_v4() not null
+        constraint vendor_product_pkey
+            primary key,
+    name      text                            not null,
+    type      vendor.product_type             not null,
+    vendor_id uuid                            not null
+        constraint vendor_product_vendor_id_fk_vendor_vendor_id
+            references vendor.vendor
+            on delete cascade
+            deferrable initially deferred,
+    color_id  uuid                            not null
+        constraint vendor_product_color_id_fk_vendor_vendor_color_id
+            references vendor.vendor_color
+            on delete cascade
+            deferrable initially deferred,
+    url       text
 );
+
+
