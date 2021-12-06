2c94a3d8ba89573f0ad87cce6a5fe5ef20b78e86
Ilya Kochankov <fakafafakafa@mail.ru>
2021-12-06 04:55:14 +0300

    update db scheme (#87)
    * update db scheme
    
    * fix ci/cd
    
    * add updated diagram
    
    * Restyle update db scheme (#88)
    
    * Restyled by pg_format
    
    * Restyled by whitespace
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- backend/db/db_schema.sql
+++ backend/db/db_schema.sql
@@ -1,36 +1,33 @@
--- Создаем отдельную схему для производителей чтобы не перемешалось с сущностями Django
 CREATE SCHEMA IF NOT EXISTS vendor;
--- Подключение расширения с поддержкой генерации uuid
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- Производители косметики
-CREATE TABLE IF NOT EXISTS vendor.vendor
-(
-    id   uuid NOT NULL DEFAULT uuid_generate_v4(),
-    name text NOT NULL,
-    url  character varying(200),
-    CONSTRAINT vendor_vendor_pkey PRIMARY KEY (id)
+
+DO $$
+BEGIN
+    CREATE TYPE vendor.product_type AS ENUM ( 'TONAL_CREAM'
 );
--- Создание перечисления для выбора типа продукта косметики
-DO
-$$
-    BEGIN
-        CREATE TYPE vendor.product_type AS ENUM ('TONAL_CREAM');
-    EXCEPTION
-        WHEN duplicate_object THEN null;
-    END
-$$;
--- Продукты косметики
-CREATE TABLE IF NOT EXISTS vendor.product
-(
-    id        uuid                  NOT NULL DEFAULT uuid_generate_v4(),
-    name      text                  NOT NULL,
-    type      vendor.product_type   NOT NULL,
-    color     character varying(18) NOT NULL,
-    vendor_id uuid                  NOT NULL,
-    CONSTRAINT vendor_product_pkey PRIMARY KEY (id),
-    CONSTRAINT vendor_product_vendor_id_fk_vendor_vendor_id FOREIGN KEY (vendor_id)
-        REFERENCES vendor.vendor (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED
+EXCEPTION
+WHEN duplicate_object THEN
+    NULL;
+END $$;
+
+CREATE TABLE IF NOT EXISTS vendor.vendor (
+        id uuid DEFAULT uuid_generate_v4 () NOT NULL CONSTRAINT vendor_vendor_pkey PRIMARY KEY,
+        name text NOT NULL,
+        url varchar(200)
+);
+
+CREATE TABLE IF NOT EXISTS vendor.vendor_color (
+        id uuid DEFAULT uuid_generate_v4 () NOT NULL CONSTRAINT vendor_color_pk PRIMARY KEY,
+        name text NOT NULL,
+        color varchar(18) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS vendor.product (
+        id uuid DEFAULT uuid_generate_v4 () NOT NULL CONSTRAINT vendor_product_pkey PRIMARY KEY,
+        name text NOT NULL,
+        TYPE vendor.product_type NOT NULL,
+        vendor_id uuid NOT NULL CONSTRAINT vendor_product_vendor_id_fk_vendor_vendor_id REFERENCES vendor.vendor ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+        color_id uuid NOT NULL CONSTRAINT vendor_product_color_id_fk_vendor_vendor_color_id REFERENCES vendor.vendor_color ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+        url text
 );
