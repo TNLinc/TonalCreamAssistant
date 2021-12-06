819c942fbc8a55f19b8d78ba313879a4baffefcf
ilyakochankov <ilyakochankov@yandex.ru>
2021-12-06 04:29:32 +0300

    update db scheme

--- backend/db/db_schema.sql
+++ backend/db/db_schema.sql
@@ -1,16 +1,7 @@
--- Создаем отдельную схему для производителей чтобы не перемешалось с сущностями Django
 CREATE SCHEMA IF NOT EXISTS vendor;
--- Подключение расширения с поддержкой генерации uuid
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- Производители косметики
-CREATE TABLE IF NOT EXISTS vendor.vendor
-(
-    id   uuid NOT NULL DEFAULT uuid_generate_v4(),
-    name text NOT NULL,
-    url  character varying(200),
-    CONSTRAINT vendor_vendor_pkey PRIMARY KEY (id)
-);
--- Создание перечисления для выбора типа продукта косметики
+
 DO
 $$
     BEGIN
@@ -19,18 +10,42 @@ $$
         WHEN duplicate_object THEN null;
     END
 $$;
--- Продукты косметики
-CREATE TABLE IF NOT EXISTS vendor.product
+
+create table if not exists vendor.vendor
 (
-    id        uuid                  NOT NULL DEFAULT uuid_generate_v4(),
-    name      text                  NOT NULL,
-    type      vendor.product_type   NOT NULL,
-    color     character varying(18) NOT NULL,
-    vendor_id uuid                  NOT NULL,
-    CONSTRAINT vendor_product_pkey PRIMARY KEY (id),
-    CONSTRAINT vendor_product_vendor_id_fk_vendor_vendor_id FOREIGN KEY (vendor_id)
-        REFERENCES vendor.vendor (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED
+    id   uuid default uuid_generate_v4() not null
+        constraint vendor_vendor_pkey
+            primary key,
+    name text                            not null,
+    url  varchar(200)
 );
+
+create table if not exists vendor.vendor_color
+(
+    id    uuid default uuid_generate_v4() not null
+        constraint vendor_color_pk
+            primary key,
+    name  text                            not null,
+    color varchar(18)                     not null
+);
+
+create table if not exists vendor.product
+(
+    id        uuid default uuid_generate_v4() not null
+        constraint vendor_product_pkey
+            primary key,
+    name      text                            not null,
+    type      vendor.product_type             not null,
+    vendor_id uuid                            not null
+        constraint vendor_product_vendor_id_fk_vendor_vendor_id
+            references vendor.vendor
+            on delete cascade
+            deferrable initially deferred,
+    color_id  uuid                            not null
+        constraint vendor_product_color_id_fk_vendor_vendor_color_id
+            references vendor.vendor_color
+            on delete cascade
+            deferrable initially deferred,
+    url       text
+);
+
